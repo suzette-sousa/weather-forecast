aa2419333788f4e737ccbcc8eba1e84f40191c46
Suzette Sousa <sousa.suzette.pro@gmail.com>
2020-12-14 10:22:50 +0100

    prepare call api

--- src/App.js
+++ src/App.js
@@ -3,13 +3,35 @@ import './App.css';
 import Heading from './components/Heading';
 import Form from './components/Form';
 
-function App() {
-  return (
-    <div className="App">
-      <Heading />
-      <Form />
-    </div>
-  );
+const api_key = "";
+
+class App extends React.Component {
+  state = {
+    temperature: "",
+    city: "",
+    country: "",
+    humidity: "",
+    pressure: "",
+    icon: "",
+    description: "",
+    error: ""
+  }
+
+  getWeather = async (e) => {
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    e.preventDefault();
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${api_key}`);
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <Heading />
+        <Form />
+      </div>
+    );
+  }
 }
 
 export default App;
